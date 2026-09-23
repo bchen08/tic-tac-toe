c72006167154428fee338cf8111620649406a2d6
bryan <bryanchen2026@gmail.com>
2026-09-22 23:57:04 -0400

    add computer play functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Square({value, onSquareClick}) {
   return (
@@ -11,10 +11,38 @@ function Square({value, onSquareClick}) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [vsComputer, setVsComputer] = useState(false);
+  
+  const winner = calculateWinner(squares);
+  const isDraw = !winner && squares.every((square) => square !==null);
+  const gameOver = winner || isDraw;
+  const computersTurn = vsComputer && !xIsNext;
+
+  useEffect(() => { {/* runs AFTER player */}
+    if (!computersTurn || gameOver) { {/* do nothing condition for computer */}
+      return;
+    }
+
+    const emptySquares = [];
+    for (let i = 0; i < squares.length; i++) { 
+      if (squares[i] === null) {
+        emptySquares.push(i); {/*if a square is empty, add its position to list*/}
+      }
+    }
+
+    const move = emptySquares[Math.floor(Math.random() * emptySquares.length)]; {/* produces an integer 0-<list length>*/}
+    const timer = setTimeout(() => {
+      const nextSquares = squares.slice();
+      nextSquares[move] = "O";
+      setSquares(nextSquares);
+      setXIsNext(true); {/*turn goes BACK to X */}
+    }, 500); {/*ms*/} 
+    }, [computersTurn, gameOver, squares]); 
+
 
   function handleClick(i) {
   
-    if (squares[i] || calculateWinner(squares)) {
+    if (squares[i] || gameOver || computersTurn) { {/*do nothing condition for player */}
       return;
     }
     const nextSquares = squares.slice();
@@ -27,22 +55,36 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
-  const winner = calculateWinner(squares);
+
   let status;
   if (winner) {
     status = "Winner: " + winner;
+  } else if (isDraw) {
+    status = "It's a draw!"; 
+  } else if (computersTurn) {
+    status = "Computer is thinking...";
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
+   }
+
+  function toggleMode() {
+    setVsComputer(!vsComputer);
+    setSquares(Array(9).fill(null)); {/*resets board to X first move*/}
+    setXIsNext(true);
   }
-      
+    
   return ( 
   <> 
-    <div className="status">{status}</div>
-    <div className="board-row">
+    <div className="status">{status} {/*status of game*/}
+    </div> 
+    <button className="mode" onClick={toggleMode}> {/*toggle button*/}
+      {vsComputer ? "Switch to 2-player mode" : "Switch to vs. computer"}
+    </button>
+    <div className="board-row"> {/*board*/}
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
       <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
+    </div>
     <div className="board-row">
       <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
       <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
